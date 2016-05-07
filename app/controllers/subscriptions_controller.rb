8d3d47922a86f3d80317cf29041e3753a300564a
Taekeun Oh <comodo872@gmail.com>
2016-05-07 11:52:49 +0900

    taetae

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,14 @@
 class SubscriptionsController < ApplicationController
   def create
     # render text: params
+    if current_user
     @subscription = Subscription.create(user_id: params[:user_id], video_id: params[:video_id])
     redirect_to :back, notice: "New video was successfully added to your list"
+    else
+      redirect_to :back, notice: "You should login first"
+    end
   end
+
 
   def destroy
     @subscription = Subscription.find_by(user_id: params[:user_id], video_id: params[:video_id])
